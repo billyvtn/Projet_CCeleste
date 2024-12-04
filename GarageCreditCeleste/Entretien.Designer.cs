28c5401d767aaeef52dcf35453a57c2c7c5774bc
BaptBers <b.bersuder2605@gmail.com>
2024-12-04 15:45:02 +0100

    renommage entretien et reparation

--- GarageCreditCeleste/Entretien.Designer.cs
+++ GarageCreditCeleste/Entretien.Designer.cs
@@ -30,36 +30,36 @@ namespace GarageCreditCeleste
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.lblImmat = new System.Windows.Forms.Label();
+            this.lblMarque = new System.Windows.Forms.Label();
+            this.lblModele = new System.Windows.Forms.Label();
+            this.lblAnnee = new System.Windows.Forms.Label();
+            this.lblKilometrage = new System.Windows.Forms.Label();
+            this.lblPuissance = new System.Windows.Forms.Label();
+            this.lblCouleur = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label9 = new System.Windows.Forms.Label();
+            this.btnFermer = new System.Windows.Forms.Button();
+            this.lblDateHeureEntretien = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label12 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-            this.button1 = new System.Windows.Forms.Button();
+            this.cboHeure = new System.Windows.Forms.ComboBox();
+            this.calEntretien = new System.Windows.Forms.MonthCalendar();
+            this.btnEnregistrer = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.lblImmat);
+            this.groupBox1.Controls.Add(this.lblMarque);
+            this.groupBox1.Controls.Add(this.lblModele);
+            this.groupBox1.Controls.Add(this.lblAnnee);
+            this.groupBox1.Controls.Add(this.lblKilometrage);
+            this.groupBox1.Controls.Add(this.lblPuissance);
+            this.groupBox1.Controls.Add(this.lblCouleur);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
             this.groupBox1.Location = new System.Drawing.Point(15, 52);
             this.groupBox1.Name = "groupBox1";
@@ -68,75 +68,75 @@ namespace GarageCreditCeleste
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Informations du véhicule";
             // 
-            // label3
+            // lblImmat
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label3.Location = new System.Drawing.Point(36, 336);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 17);
-            this.label3.TabIndex = 45;
-            this.label3.Text = "Immatriculation : ...";
+            this.lblImmat.AutoSize = true;
+            this.lblImmat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblImmat.Location = new System.Drawing.Point(36, 336);
+            this.lblImmat.Name = "lblImmat";
+            this.lblImmat.Size = new System.Drawing.Size(110, 17);
+            this.lblImmat.TabIndex = 45;
+            this.lblImmat.Text = "Immatriculation :";
             // 
-            // label4
+            // lblMarque
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label4.Location = new System.Drawing.Point(36, 39);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 17);
-            this.label4.TabIndex = 30;
-            this.label4.Text = "Marque : ...";
+            this.lblMarque.AutoSize = true;
+            this.lblMarque.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblMarque.Location = new System.Drawing.Point(36, 39);
+            this.lblMarque.Name = "lblMarque";
+            this.lblMarque.Size = new System.Drawing.Size(64, 17);
+            this.lblMarque.TabIndex = 30;
+            this.lblMarque.Text = "Marque :";
             // 
-            // label5
+            // lblModele
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label5.Location = new System.Drawing.Point(36, 89);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(78, 17);
-            this.label5.TabIndex = 31;
-            this.label5.Text = "Modèle : ...";
+            this.lblModele.AutoSize = true;
+            this.lblModele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblModele.Location = new System.Drawing.Point(36, 89);
+            this.lblModele.Name = "lblModele";
+            this.lblModele.Size = new System.Drawing.Size(62, 17);
+            this.lblModele.TabIndex = 31;
+            this.lblModele.Text = "Modèle :";
             // 
-            // label6
+            // lblAnnee
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label6.Location = new System.Drawing.Point(36, 139);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(73, 17);
-            this.label6.TabIndex = 32;
-            this.label6.Text = "Année : ...";
+            this.lblAnnee.AutoSize = true;
+            this.lblAnnee.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblAnnee.Location = new System.Drawing.Point(36, 139);
+            this.lblAnnee.Name = "lblAnnee";
+            this.lblAnnee.Size = new System.Drawing.Size(57, 17);
+            this.lblAnnee.TabIndex = 32;
+            this.lblAnnee.Text = "Année :";
             // 
-            // label7
+            // lblKilometrage
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label7.Location = new System.Drawing.Point(36, 189);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(107, 17);
-            this.label7.TabIndex = 33;
-            this.label7.Text = "Kilométrage : ...";
+            this.lblKilometrage.AutoSize = true;
+            this.lblKilometrage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblKilometrage.Location = new System.Drawing.Point(36, 189);
+            this.lblKilometrage.Name = "lblKilometrage";
+            this.lblKilometrage.Size = new System.Drawing.Size(91, 17);
+            this.lblKilometrage.TabIndex = 33;
+            this.lblKilometrage.Text = "Kilométrage :";
             // 
-            // label10
+            // lblPuissance
             // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label10.Location = new System.Drawing.Point(36, 286);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(97, 17);
-            this.label10.TabIndex = 39;
-            this.label10.Text = "Puissance : ...";
+            this.lblPuissance.AutoSize = true;
+            this.lblPuissance.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblPuissance.Location = new System.Drawing.Point(36, 286);
+            this.lblPuissance.Name = "lblPuissance";
+            this.lblPuissance.Size = new System.Drawing.Size(81, 17);
+            this.lblPuissance.TabIndex = 39;
+            this.lblPuissance.Text = "Puissance :";
             // 
-            // label11
+            // lblCouleur
             // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label11.Location = new System.Drawing.Point(36, 236);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(81, 17);
-            this.label11.TabIndex = 38;
-            this.label11.Text = "Couleur : ...";
+            this.lblCouleur.AutoSize = true;
+            this.lblCouleur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblCouleur.Location = new System.Drawing.Point(36, 236);
+            this.lblCouleur.Name = "lblCouleur";
+            this.lblCouleur.Size = new System.Drawing.Size(65, 17);
+            this.lblCouleur.TabIndex = 38;
+            this.lblCouleur.Text = "Couleur :";
             // 
             // label8
             // 
@@ -154,9 +154,9 @@ namespace GarageCreditCeleste
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
             this.label2.Location = new System.Drawing.Point(698, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.Size = new System.Drawing.Size(94, 18);
             this.label2.TabIndex = 66;
-            this.label2.Text = "Nom garage";
+            this.label2.Text = "HDR Garage";
             // 
             // label1
             // 
@@ -164,36 +164,36 @@ namespace GarageCreditCeleste
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
             this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 18);
+            this.label1.Size = new System.Drawing.Size(81, 18);
             this.label1.TabIndex = 65;
-            this.label1.Text = "Ville";
+            this.label1.Text = "Strasbourg";
             // 
-            // button4
+            // btnFermer
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.button4.Location = new System.Drawing.Point(698, 409);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(90, 30);
-            this.button4.TabIndex = 68;
-            this.button4.Text = "Fermer";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnFermer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.btnFermer.Location = new System.Drawing.Point(698, 409);
+            this.btnFermer.Name = "btnFermer";
+            this.btnFermer.Size = new System.Drawing.Size(90, 30);
+            this.btnFermer.TabIndex = 68;
+            this.btnFermer.Text = "Fermer";
+            this.btnFermer.UseVisualStyleBackColor = true;
             // 
-            // label9
+            // lblDateHeureEntretien
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label9.Location = new System.Drawing.Point(16, 263);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(359, 17);
-            this.label9.TabIndex = 46;
-            this.label9.Text = "Date et heure du rendez-vous : Jour Mois Année Heure";
+            this.lblDateHeureEntretien.AutoSize = true;
+            this.lblDateHeureEntretien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblDateHeureEntretien.Location = new System.Drawing.Point(16, 263);
+            this.lblDateHeureEntretien.Name = "lblDateHeureEntretien";
+            this.lblDateHeureEntretien.Size = new System.Drawing.Size(210, 17);
+            this.lblDateHeureEntretien.TabIndex = 46;
+            this.lblDateHeureEntretien.Text = "Date et heure du rendez-vous : ";
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label12);
-            this.groupBox2.Controls.Add(this.comboBox1);
-            this.groupBox2.Controls.Add(this.label9);
-            this.groupBox2.Controls.Add(this.monthCalendar1);
+            this.groupBox2.Controls.Add(this.cboHeure);
+            this.groupBox2.Controls.Add(this.lblDateHeureEntretien);
+            this.groupBox2.Controls.Add(this.calEntretien);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
             this.groupBox2.Location = new System.Drawing.Point(395, 52);
             this.groupBox2.Name = "groupBox2";
@@ -212,30 +212,30 @@ namespace GarageCreditCeleste
             this.label12.TabIndex = 46;
             this.label12.Text = "Heure :";
             // 
-            // comboBox1
+            // cboHeure
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(170, 221);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(124, 25);
-            this.comboBox1.TabIndex = 56;
+            this.cboHeure.FormattingEnabled = true;
+            this.cboHeure.Location = new System.Drawing.Point(170, 221);
+            this.cboHeure.Name = "cboHeure";
+            this.cboHeure.Size = new System.Drawing.Size(124, 25);
+            this.cboHeure.TabIndex = 56;
             // 
-            // monthCalendar1
+            // calEntretien
             // 
-            this.monthCalendar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.monthCalendar1.Location = new System.Drawing.Point(92, 28);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 55;
+            this.calEntretien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.calEntretien.Location = new System.Drawing.Point(92, 28);
+            this.calEntretien.Name = "calEntretien";
+            this.calEntretien.TabIndex = 55;
             // 
-            // button1
+            // btnEnregistrer
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.button1.Location = new System.Drawing.Point(602, 409);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 30);
-            this.button1.TabIndex = 71;
-            this.button1.Text = "Enregistrer";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEnregistrer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.btnEnregistrer.Location = new System.Drawing.Point(602, 409);
+            this.btnEnregistrer.Name = "btnEnregistrer";
+            this.btnEnregistrer.Size = new System.Drawing.Size(90, 30);
+            this.btnEnregistrer.TabIndex = 71;
+            this.btnEnregistrer.Text = "Enregistrer";
+            this.btnEnregistrer.UseVisualStyleBackColor = true;
             // 
             // Entretien
             // 
@@ -246,9 +246,9 @@ namespace GarageCreditCeleste
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnFermer);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEnregistrer);
             this.Name = "Entretien";
             this.Text = "Entretien";
             this.groupBox1.ResumeLayout(false);
@@ -263,22 +263,22 @@ namespace GarageCreditCeleste
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblImmat;
+        private System.Windows.Forms.Label lblMarque;
+        private System.Windows.Forms.Label lblModele;
+        private System.Windows.Forms.Label lblAnnee;
+        private System.Windows.Forms.Label lblKilometrage;
+        private System.Windows.Forms.Label lblPuissance;
+        private System.Windows.Forms.Label lblCouleur;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnFermer;
+        private System.Windows.Forms.Label lblDateHeureEntretien;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.MonthCalendar monthCalendar1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboHeure;
+        private System.Windows.Forms.MonthCalendar calEntretien;
+        private System.Windows.Forms.Button btnEnregistrer;
     }
 }
