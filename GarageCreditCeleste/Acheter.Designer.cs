c1e35ec8549cbefd1eae204ec127e87d73da3214
BaptBers <b.bersuder2605@gmail.com>
2024-12-04 13:59:55 +0100

    renommage acheter

--- GarageCreditCeleste/Acheter.Designer.cs
+++ GarageCreditCeleste/Acheter.Designer.cs
@@ -30,12 +30,12 @@ namespace GarageCreditCeleste
         private void InitializeComponent()
         {
             this.label8 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.gpbDescription = new System.Windows.Forms.GroupBox();
+            this.btnFiltrer = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.lsbVoituresDispo = new System.Windows.Forms.ListBox();
+            this.btnAcheter = new System.Windows.Forms.Button();
+            this.btnFermer = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -50,25 +50,25 @@ namespace GarageCreditCeleste
             this.label8.TabIndex = 29;
             this.label8.Text = "Acheter";
             // 
-            // groupBox1
+            // gpbDescription
             // 
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.groupBox1.Location = new System.Drawing.Point(295, 55);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(493, 338);
-            this.groupBox1.TabIndex = 28;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Description du véhicule";
+            this.gpbDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.gpbDescription.Location = new System.Drawing.Point(295, 55);
+            this.gpbDescription.Name = "gpbDescription";
+            this.gpbDescription.Size = new System.Drawing.Size(493, 338);
+            this.gpbDescription.TabIndex = 28;
+            this.gpbDescription.TabStop = false;
+            this.gpbDescription.Text = "Description du véhicule";
             // 
-            // button2
+            // btnFiltrer
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.button2.Location = new System.Drawing.Point(219, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 25);
-            this.button2.TabIndex = 27;
-            this.button2.Text = "Filtrer";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnFiltrer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.btnFiltrer.Location = new System.Drawing.Point(219, 41);
+            this.btnFiltrer.Name = "btnFiltrer";
+            this.btnFiltrer.Size = new System.Drawing.Size(70, 25);
+            this.btnFiltrer.TabIndex = 27;
+            this.btnFiltrer.Text = "Filtrer";
+            this.btnFiltrer.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -80,35 +80,35 @@ namespace GarageCreditCeleste
             this.label3.TabIndex = 26;
             this.label3.Text = "Voitures disponibles";
             // 
-            // listBox1
+            // lsbVoituresDispo
             // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 18;
-            this.listBox1.Location = new System.Drawing.Point(15, 65);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(274, 328);
-            this.listBox1.TabIndex = 25;
+            this.lsbVoituresDispo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.lsbVoituresDispo.FormattingEnabled = true;
+            this.lsbVoituresDispo.ItemHeight = 18;
+            this.lsbVoituresDispo.Location = new System.Drawing.Point(15, 65);
+            this.lsbVoituresDispo.Name = "lsbVoituresDispo";
+            this.lsbVoituresDispo.Size = new System.Drawing.Size(274, 328);
+            this.lsbVoituresDispo.TabIndex = 25;
             // 
-            // button1
+            // btnAcheter
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.button1.Location = new System.Drawing.Point(602, 410);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 30);
-            this.button1.TabIndex = 24;
-            this.button1.Text = "Acheter";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAcheter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.btnAcheter.Location = new System.Drawing.Point(602, 410);
+            this.btnAcheter.Name = "btnAcheter";
+            this.btnAcheter.Size = new System.Drawing.Size(90, 30);
+            this.btnAcheter.TabIndex = 24;
+            this.btnAcheter.Text = "Acheter";
+            this.btnAcheter.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnFermer
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.button4.Location = new System.Drawing.Point(698, 410);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(90, 30);
-            this.button4.TabIndex = 23;
-            this.button4.Text = "Fermer";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnFermer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.btnFermer.Location = new System.Drawing.Point(698, 410);
+            this.btnFermer.Name = "btnFermer";
+            this.btnFermer.Size = new System.Drawing.Size(90, 30);
+            this.btnFermer.TabIndex = 23;
+            this.btnFermer.Text = "Fermer";
+            this.btnFermer.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -116,9 +116,9 @@ namespace GarageCreditCeleste
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
             this.label2.Location = new System.Drawing.Point(698, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.Size = new System.Drawing.Size(94, 18);
             this.label2.TabIndex = 22;
-            this.label2.Text = "Nom garage";
+            this.label2.Text = "HDR Garage";
             // 
             // label1
             // 
@@ -126,9 +126,9 @@ namespace GarageCreditCeleste
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
             this.label1.Location = new System.Drawing.Point(12, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 18);
+            this.label1.Size = new System.Drawing.Size(81, 18);
             this.label1.TabIndex = 21;
-            this.label1.Text = "Ville";
+            this.label1.Text = "Strasbourg";
             // 
             // Acheter
             // 
@@ -136,12 +136,12 @@ namespace GarageCreditCeleste
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.gpbDescription);
+            this.Controls.Add(this.btnFiltrer);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lsbVoituresDispo);
+            this.Controls.Add(this.btnAcheter);
+            this.Controls.Add(this.btnFermer);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Acheter";
@@ -154,12 +154,12 @@ namespace GarageCreditCeleste
         #endregion
 
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox gpbDescription;
+        private System.Windows.Forms.Button btnFiltrer;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox lsbVoituresDispo;
+        private System.Windows.Forms.Button btnAcheter;
+        private System.Windows.Forms.Button btnFermer;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
