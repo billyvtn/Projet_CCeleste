13edae773561e43adad1884949a60cba499d11c3
billyvtn <billypham67@gmail.com>
2025-04-19 21:52:22 +0200

    Corrextion tab index fenetre filtre

--- GarageCreditCeleste/Filtre.Designer.cs
+++ GarageCreditCeleste/Filtre.Designer.cs
@@ -44,10 +44,10 @@ namespace GarageCreditCeleste
             this.txtAnneeMaxi = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAnneeMini = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.groupBox6 = new System.Windows.Forms.GroupBox();
             this.txtCouleur = new System.Windows.Forms.TextBox();
-            this.txtAnneeMini = new System.Windows.Forms.TextBox();
             this.groupBox5.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -59,11 +59,10 @@ namespace GarageCreditCeleste
             // btnFermer
             // 
             this.btnFermer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.btnFermer.Location = new System.Drawing.Point(384, 602);
-            this.btnFermer.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFermer.Location = new System.Drawing.Point(288, 489);
             this.btnFermer.Name = "btnFermer";
-            this.btnFermer.Size = new System.Drawing.Size(112, 34);
-            this.btnFermer.TabIndex = 44;
+            this.btnFermer.Size = new System.Drawing.Size(84, 28);
+            this.btnFermer.TabIndex = 11;
             this.btnFermer.Text = "Fermer";
             this.btnFermer.UseVisualStyleBackColor = true;
             this.btnFermer.Click += new System.EventHandler(this.btnFermer_Click);
@@ -71,22 +70,20 @@ namespace GarageCreditCeleste
             // txtPuissanceMaxi
             // 
             this.txtPuissanceMaxi.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtPuissanceMaxi.Location = new System.Drawing.Point(251, 33);
-            this.txtPuissanceMaxi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPuissanceMaxi.Location = new System.Drawing.Point(188, 27);
             this.txtPuissanceMaxi.Name = "txtPuissanceMaxi";
-            this.txtPuissanceMaxi.Size = new System.Drawing.Size(151, 27);
-            this.txtPuissanceMaxi.TabIndex = 1;
+            this.txtPuissanceMaxi.Size = new System.Drawing.Size(114, 23);
+            this.txtPuissanceMaxi.TabIndex = 8;
             this.txtPuissanceMaxi.Text = "Maximum";
             this.txtPuissanceMaxi.Click += new System.EventHandler(this.txtPuissanceMaxi_Click);
             // 
             // txtPuissanceMini
             // 
             this.txtPuissanceMini.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtPuissanceMini.Location = new System.Drawing.Point(75, 33);
-            this.txtPuissanceMini.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPuissanceMini.Location = new System.Drawing.Point(56, 27);
             this.txtPuissanceMini.Name = "txtPuissanceMini";
-            this.txtPuissanceMini.Size = new System.Drawing.Size(151, 27);
-            this.txtPuissanceMini.TabIndex = 0;
+            this.txtPuissanceMini.Size = new System.Drawing.Size(114, 23);
+            this.txtPuissanceMini.TabIndex = 7;
             this.txtPuissanceMini.Text = "Minimum";
             this.txtPuissanceMini.Click += new System.EventHandler(this.txtPuissanceMini_Click);
             // 
@@ -95,45 +92,40 @@ namespace GarageCreditCeleste
             this.groupBox5.Controls.Add(this.txtPuissanceMaxi);
             this.groupBox5.Controls.Add(this.txtPuissanceMini);
             this.groupBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.groupBox5.Location = new System.Drawing.Point(16, 414);
-            this.groupBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox5.Location = new System.Drawing.Point(12, 336);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox5.Size = new System.Drawing.Size(480, 86);
-            this.groupBox5.TabIndex = 42;
+            this.groupBox5.Size = new System.Drawing.Size(360, 70);
+            this.groupBox5.TabIndex = 5;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Puissance";
             // 
             // txtKilometrageMaxi
             // 
             this.txtKilometrageMaxi.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtKilometrageMaxi.Location = new System.Drawing.Point(251, 33);
-            this.txtKilometrageMaxi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKilometrageMaxi.Location = new System.Drawing.Point(188, 27);
             this.txtKilometrageMaxi.Name = "txtKilometrageMaxi";
-            this.txtKilometrageMaxi.Size = new System.Drawing.Size(151, 27);
-            this.txtKilometrageMaxi.TabIndex = 1;
+            this.txtKilometrageMaxi.Size = new System.Drawing.Size(114, 23);
+            this.txtKilometrageMaxi.TabIndex = 6;
             this.txtKilometrageMaxi.Text = "Maximum";
             this.txtKilometrageMaxi.Click += new System.EventHandler(this.txtKilometrageMaxi_Click);
             // 
             // txtKilometrageMini
             // 
             this.txtKilometrageMini.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtKilometrageMini.Location = new System.Drawing.Point(75, 33);
-            this.txtKilometrageMini.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKilometrageMini.Location = new System.Drawing.Point(56, 27);
             this.txtKilometrageMini.Name = "txtKilometrageMini";
-            this.txtKilometrageMini.Size = new System.Drawing.Size(151, 27);
-            this.txtKilometrageMini.TabIndex = 0;
+            this.txtKilometrageMini.Size = new System.Drawing.Size(114, 23);
+            this.txtKilometrageMini.TabIndex = 5;
             this.txtKilometrageMini.Text = "Minimum";
             this.txtKilometrageMini.Click += new System.EventHandler(this.txtKilometrageMini_Click);
             // 
             // btnAppliquer
             // 
             this.btnAppliquer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.btnAppliquer.Location = new System.Drawing.Point(264, 602);
-            this.btnAppliquer.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAppliquer.Location = new System.Drawing.Point(198, 489);
             this.btnAppliquer.Name = "btnAppliquer";
-            this.btnAppliquer.Size = new System.Drawing.Size(112, 34);
-            this.btnAppliquer.TabIndex = 43;
+            this.btnAppliquer.Size = new System.Drawing.Size(84, 28);
+            this.btnAppliquer.TabIndex = 10;
             this.btnAppliquer.Text = "Appliquer";
             this.btnAppliquer.UseVisualStyleBackColor = true;
             this.btnAppliquer.Click += new System.EventHandler(this.btnAppliquer_Click);
@@ -143,43 +135,38 @@ namespace GarageCreditCeleste
             this.groupBox4.Controls.Add(this.txtKilometrageMaxi);
             this.groupBox4.Controls.Add(this.txtKilometrageMini);
             this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.groupBox4.Location = new System.Drawing.Point(16, 320);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox4.Location = new System.Drawing.Point(12, 260);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox4.Size = new System.Drawing.Size(480, 86);
-            this.groupBox4.TabIndex = 39;
+            this.groupBox4.Size = new System.Drawing.Size(360, 70);
+            this.groupBox4.TabIndex = 4;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Kilométrage";
             // 
             // cboMarque
             // 
             this.cboMarque.FormattingEnabled = true;
-            this.cboMarque.Location = new System.Drawing.Point(124, 33);
-            this.cboMarque.Margin = new System.Windows.Forms.Padding(4);
+            this.cboMarque.Location = new System.Drawing.Point(93, 27);
             this.cboMarque.Name = "cboMarque";
-            this.cboMarque.Size = new System.Drawing.Size(229, 28);
+            this.cboMarque.Size = new System.Drawing.Size(173, 25);
             this.cboMarque.TabIndex = 0;
             // 
             // txtPrixMaxi
             // 
             this.txtPrixMaxi.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtPrixMaxi.Location = new System.Drawing.Point(251, 33);
-            this.txtPrixMaxi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrixMaxi.Location = new System.Drawing.Point(188, 27);
             this.txtPrixMaxi.Name = "txtPrixMaxi";
-            this.txtPrixMaxi.Size = new System.Drawing.Size(151, 27);
-            this.txtPrixMaxi.TabIndex = 1;
+            this.txtPrixMaxi.Size = new System.Drawing.Size(114, 23);
+            this.txtPrixMaxi.TabIndex = 2;
             this.txtPrixMaxi.Text = "Maximum";
             this.txtPrixMaxi.Click += new System.EventHandler(this.txtPrixMaxi_Click);
             // 
             // txtPrixMini
             // 
             this.txtPrixMini.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtPrixMini.Location = new System.Drawing.Point(75, 33);
-            this.txtPrixMini.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrixMini.Location = new System.Drawing.Point(56, 27);
             this.txtPrixMini.Name = "txtPrixMini";
-            this.txtPrixMini.Size = new System.Drawing.Size(151, 27);
-            this.txtPrixMini.TabIndex = 0;
+            this.txtPrixMini.Size = new System.Drawing.Size(114, 23);
+            this.txtPrixMini.TabIndex = 1;
             this.txtPrixMini.Text = "Minimum";
             this.txtPrixMini.Click += new System.EventHandler(this.txtPrixMini_Click);
             this.txtPrixMini.TextChanged += new System.EventHandler(this.txtPrixMini_TextChanged);
@@ -189,23 +176,20 @@ namespace GarageCreditCeleste
             this.groupBox2.Controls.Add(this.txtPrixMaxi);
             this.groupBox2.Controls.Add(this.txtPrixMini);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.groupBox2.Location = new System.Drawing.Point(16, 133);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 108);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(480, 86);
-            this.groupBox2.TabIndex = 40;
+            this.groupBox2.Size = new System.Drawing.Size(360, 70);
+            this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Prix";
             // 
             // txtAnneeMaxi
             // 
             this.txtAnneeMaxi.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtAnneeMaxi.Location = new System.Drawing.Point(251, 33);
-            this.txtAnneeMaxi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAnneeMaxi.Location = new System.Drawing.Point(188, 27);
             this.txtAnneeMaxi.Name = "txtAnneeMaxi";
-            this.txtAnneeMaxi.Size = new System.Drawing.Size(151, 27);
-            this.txtAnneeMaxi.TabIndex = 1;
+            this.txtAnneeMaxi.Size = new System.Drawing.Size(114, 23);
+            this.txtAnneeMaxi.TabIndex = 4;
             this.txtAnneeMaxi.Text = "Maximum";
             this.txtAnneeMaxi.Click += new System.EventHandler(this.txtAnneeMaxi_Click);
             // 
@@ -213,12 +197,10 @@ namespace GarageCreditCeleste
             // 
             this.groupBox3.Controls.Add(this.cboMarque);
             this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.groupBox3.Location = new System.Drawing.Point(16, 39);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Location = new System.Drawing.Point(12, 32);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox3.Size = new System.Drawing.Size(480, 86);
-            this.groupBox3.TabIndex = 41;
+            this.groupBox3.Size = new System.Drawing.Size(360, 70);
+            this.groupBox3.TabIndex = 1;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Marque";
             // 
@@ -227,23 +209,30 @@ namespace GarageCreditCeleste
             this.groupBox1.Controls.Add(this.txtAnneeMaxi);
             this.groupBox1.Controls.Add(this.txtAnneeMini);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.groupBox1.Location = new System.Drawing.Point(16, 226);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 184);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(480, 86);
-            this.groupBox1.TabIndex = 38;
+            this.groupBox1.Size = new System.Drawing.Size(360, 70);
+            this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Année";
+            // 
+            // txtAnneeMini
+            // 
+            this.txtAnneeMini.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtAnneeMini.Location = new System.Drawing.Point(56, 27);
+            this.txtAnneeMini.Name = "txtAnneeMini";
+            this.txtAnneeMini.Size = new System.Drawing.Size(114, 23);
+            this.txtAnneeMini.TabIndex = 3;
+            this.txtAnneeMini.Text = "Minimum";
+            this.txtAnneeMini.Click += new System.EventHandler(this.txtAnneeMini_Click);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label8.Location = new System.Drawing.Point(217, 14);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(163, 11);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(51, 24);
+            this.label8.Size = new System.Drawing.Size(40, 18);
             this.label8.TabIndex = 37;
             this.label8.Text = "Filtre";
             // 
@@ -251,42 +240,28 @@ namespace GarageCreditCeleste
             // 
             this.groupBox6.Controls.Add(this.txtCouleur);
             this.groupBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.groupBox6.Location = new System.Drawing.Point(16, 507);
-            this.groupBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox6.Location = new System.Drawing.Point(12, 412);
             this.groupBox6.Name = "groupBox6";
-            this.groupBox6.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox6.Size = new System.Drawing.Size(480, 86);
-            this.groupBox6.TabIndex = 43;
+            this.groupBox6.Size = new System.Drawing.Size(360, 70);
+            this.groupBox6.TabIndex = 6;
             this.groupBox6.TabStop = false;
             this.groupBox6.Text = "Couleur";
             // 
             // txtCouleur
             // 
             this.txtCouleur.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtCouleur.Location = new System.Drawing.Point(124, 38);
-            this.txtCouleur.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCouleur.Location = new System.Drawing.Point(93, 31);
             this.txtCouleur.Name = "txtCouleur";
-            this.txtCouleur.Size = new System.Drawing.Size(213, 27);
-            this.txtCouleur.TabIndex = 1;
+            this.txtCouleur.Size = new System.Drawing.Size(161, 23);
+            this.txtCouleur.TabIndex = 9;
             this.txtCouleur.Text = "ex. rouge, bleu, noire...";
             this.txtCouleur.Click += new System.EventHandler(this.txtCouleur_Click);
             // 
-            // txtAnneeMini
-            // 
-            this.txtAnneeMini.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtAnneeMini.Location = new System.Drawing.Point(75, 33);
-            this.txtAnneeMini.Margin = new System.Windows.Forms.Padding(4);
-            this.txtAnneeMini.Name = "txtAnneeMini";
-            this.txtAnneeMini.Size = new System.Drawing.Size(151, 27);
-            this.txtAnneeMini.TabIndex = 0;
-            this.txtAnneeMini.Text = "Minimum";
-            this.txtAnneeMini.Click += new System.EventHandler(this.txtAnneeMini_Click);
-            // 
             // Filtre
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 651);
+            this.ClientSize = new System.Drawing.Size(384, 529);
             this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.btnFermer);
             this.Controls.Add(this.groupBox5);
@@ -296,7 +271,6 @@ namespace GarageCreditCeleste
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label8);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Filtre";
             this.Text = "Filtre";
             this.Load += new System.EventHandler(this.Filtre_Load);
